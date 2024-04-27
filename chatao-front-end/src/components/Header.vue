fe83961409932ac2324475eb3c8e8e3f3e31fc12
pdrzan <pedeozanelato19@gmail.com>
2024-04-27 14:15:03 -0400

    feature: Add Header
    Header component was added
    ChatsView view was added
    The route for ChatsView was added
    User example image was added

---
+++ chatao-front-end/src/components/Header.vue
@@ -0,0 +1,53 @@
+<script setup>
+</script>
+<template>
+<div class="header">
+	<div class="logo">
+		<img class="logo-img" src="./../assets/media/logo.png" alt="chatao's logo"/>
+		<div class="logo-text"> Chatão </div>
+	</div>
+	<div class="profile">
+		<img class="profile-photo" src="./../assets/media/user.png" alt="user's photo"/>
+		<div class="profile-text"> Visualizar Perfil </div>
+	</div>
+</div>
+</template>
+<style scoped>
+.header {
+	width: 100%;
+	box-sizing: border-box;
+	height: 100px;
+	padding: 0 55px;
+
+	background-color: var(--green-light);
+	
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+}
+.logo {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 15px;
+}
+.logo-text {
+	font-size: 32px;
+	font-weight: 700;
+    text-shadow: 1px 1px 3px rgb(56, 56, 56);
+}
+.logo-img {
+	width: 84px;
+}
+.profile {
+	display: flex;
+	justify-content: center;
+	align-items: center;
+	gap: 15px;
+}
+.profile-text {
+ 	color: var(--green-dark);
+	font-size: 32px;
+	font-weight: 700;
+}
+</style>
--- chatao-front-end/src/router/index.js
+++ chatao-front-end/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import LoginView from '../views/LoginView.vue'
 import SignUpView from '../views/SignUpView.vue'
+import ChatsView from '../views/ChatsView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -12,7 +13,11 @@ const router = createRouter({
 	{
 	  path: '/signup',
 	  component: SignUpView
-	}
+	},
+	{
+	  path: '/chats',
+	  component: ChatsView
+    }
   ]
 })
 
