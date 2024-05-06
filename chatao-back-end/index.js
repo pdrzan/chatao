fc888f90db4ce2b9e8f3899aa4a01f28f00a26eb
Pedro Zanelato <pedeozanelato19@gmail.com>
2024-05-06 14:21:54 +0000

    feature: Add login

--- chatao-back-end/index.js
+++ chatao-back-end/index.js
@@ -1,10 +1,41 @@
 const express = require('express') 
 const app = express()
+app.use(express.json())
 const port = 3000
 const router = express.Router()
 
-router.post('/login', (req, res) => {
-	res.send("{}")
+async function digestMessage(message) {
+	const msgUint8 = new TextEncoder().encode(message);
+	const hashBuffer = await crypto.subtle.digest("SHA-256", msgUint8);
+	const hashArray = Array.from(new Uint8Array(hashBuffer));
+	const hashHex = hashArray
+	  .map((b) => b.toString(16).padStart(2, "0"))
+	  .join("");
+	return hashHex;
+  }
+
+let active_users = {}
+
+router.post('/login', async function(req, res) {
+	let required_attrs = ["name", "password"]
+	for(let i = 0; i < required_attrs.length; i++)
+	{
+		if(!Object.hasOwn(req.body, required_attrs[i]))
+		{
+			res.status(400).send('Bad Request')
+			console.log("dict")
+			return
+		}
+	}
+	if(Object.keys(req.body).length != required_attrs.length)
+	{
+		res.status(400).send('Bad Request')
+		console.log("dict")
+		return
+	}
+	let user_logged = await digestMessage(req.body["name"] + Date.now().toString())
+	console.log({user_logged: user_logged})
+	return res.status(200).json({user_logged: user_logged})
 })
 
 router.post('/logout', (req, res) => {
