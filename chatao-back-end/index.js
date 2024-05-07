e80e10f4131d9cdeee00d13ab99bfece7c6b19d1
Pedro Zanelato <pedeozanelato19@gmail.com>
2024-05-07 18:25:41 +0000

    feature: Add logout

--- chatao-back-end/index.js
+++ chatao-back-end/index.js
@@ -12,34 +12,61 @@ async function digestMessage(message) {
 	  .map((b) => b.toString(16).padStart(2, "0"))
 	  .join("");
 	return hashHex;
-  }
+}
 
+let users = {}
 let active_users = {}
 
 router.post('/login', async function(req, res) {
 	let required_attrs = ["name", "password"]
+	let user = await digestMessage(req.body["name"])
 	for(let i = 0; i < required_attrs.length; i++)
 	{
 		if(!Object.hasOwn(req.body, required_attrs[i]))
 		{
 			res.status(400).send('Bad Request')
-			console.log("dict")
 			return
 		}
 	}
 	if(Object.keys(req.body).length != required_attrs.length)
 	{
 		res.status(400).send('Bad Request')
-		console.log("dict")
 		return
 	}
+	if(active_users[user])
+	{
+		return res.status(200).json({user_logged: active_users[user]})
+	}
 	let user_logged = await digestMessage(req.body["name"] + Date.now().toString())
-	console.log({user_logged: user_logged})
+	active_users[user] = user_logged
 	return res.status(200).json({user_logged: user_logged})
 })
 
-router.post('/logout', (req, res) => {
-	res.send("{}")
+router.post('/logout', async function (req, res) {
+	let required_attrs = ["name", "user_logged"]
+	let user = await digestMessage(req.body["name"])
+	for(let i = 0; i < required_attrs.length; i++)
+	{
+		if(!Object.hasOwn(req.body, required_attrs[i]))
+		{
+			res.status(400).send('Bad Request')
+			return
+		}
+	}
+	if(Object.keys(req.body).length != required_attrs.length)
+	{
+		res.status(400).send('Bad Request')
+		return
+	}
+	if(!active_users[user])
+	{
+		return res.status(400).send('Bad Request')
+	}
+	else
+	{
+		delete active_users[user]
+		return res.status(200).json({status: 'Success'})
+	}
 })
 
 router.post('/signup', (req, res) => {
