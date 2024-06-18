3220764b7e2607e03be273df08655ac67f3a4ea5
pdrzan <pedeozanelato19@gmail.com>
2024-06-17 20:39:36 -0400

    feat: Add chat route in message.js

--- chatao-back-end/src/routes/message.js
+++ chatao-back-end/src/routes/message.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database');
+const { Op } = require("sequelize");
 
 router.get("/all", function(req, res) {
     db.Message.findAll()
@@ -22,6 +23,47 @@ router.get("/:id", function(req, res) {
         })
 });
 
+router.put("/chat", function(req, res) {
+    db.Chat.findOne({ where: { 
+        [Op.or]: [{
+            userId_1: req.body.userId_1,
+            userId_2: req.body.userId_2 
+        }, {
+            userId_1: req.body.userId_2, 
+            userId_2: req.body.userId_1 
+        }] 
+    }})
+        .then((chat) => {
+            if(chat === null || chat === undefined) {
+                db.Chat.create({
+                    userId_1: req.body.userId_1,
+                    userId_2: req.body.userId_2
+                })
+                    .then( chat => {
+                        res.status(200).send(JSON.stringify([]));
+                    })
+                    .catch( err => {
+                        console.log(err);
+                        res.status(500).send(JSON.stringify(err));
+                    })
+            } else {
+                db.Message.findAll({ where: {
+                    chatId: chat.id
+                    }
+                })
+                .then( messages => {
+                    res.status(200).send(JSON.stringify({ chatId: chat.id, messages: messages }));
+                })
+                .catch( err => {
+                    console.log(err);
+                    res.status(500).send(JSON.stringify(err));
+                })
+            }
+        })
+});
+
+
+
 router.put("/", function(req, res) {
     db.Message.create({
         chatId: req.body.chatId,
@@ -29,7 +71,7 @@ router.put("/", function(req, res) {
         message: req.body.message,
         })
         .then( message => {
-            res.status(200).send(JSON.stringify(person));
+            res.status(200).send(JSON.stringify(message));
         })
         .catch( err => {
             res.status(500).send(JSON.stringify(err));
