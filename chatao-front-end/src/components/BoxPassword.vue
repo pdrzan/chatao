29674b4230bc9f71a7077b5a45b0bd34761e37b9
pdrzan <pedeozanelato19@gmail.com>
2024-06-17 20:44:02 -0400

    feat: Add BoxPassword component

---
+++ chatao-front-end/src/components/BoxPassword.vue
@@ -0,0 +1,22 @@
+<script setup>
+const props = defineProps({
+    id: String,
+    placeholder: String,
+    name: String,
+    minlength: Number,
+    maxlength: String,
+    required: Boolean,
+})
+</script>
+<template>
+    <input class="input" type="password" :id :placeholder :name :minlength :maxlength :required />
+</template>
+<style scoped>
+.input {
+    padding: 15px;
+    background-color: var(--green-light);
+    border: none;
+    border-radius: 15px;
+    flex-grow: 1;
+}
+</style>
