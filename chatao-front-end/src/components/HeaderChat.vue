33396f46cb2c48f6adcebc2894cbcd03fdef3ad8
pdrzan <pedeozanelato19@gmail.com>
2024-06-17 20:48:55 -0400

    feat: Add InputEmail component

--- chatao-front-end/src/components/HeaderChat.vue
+++ chatao-front-end/src/components/HeaderChat.vue
@@ -2,15 +2,15 @@
 import { RouterLink } from 'vue-router'
 </script>
 <template>
-<div class="header-chat">
-	<div class="header-contact">
-		<img src="../assets/media/user.png" alt="Contact photo" class="contact-photo" />
-		<div class="contact-name">Contato</div>
+	<div class="header-chat">
+		<div class="header-contact">
+			<img src="../assets/media/user.png" alt="Contact photo" class="contact-photo" />
+			<div class="contact-name">Contato</div>
+		</div>
+		<RouterLink to="/chats/">
+			<img src="../assets/media/close.png" alt="Close image" class="close-img" />
+		</RouterLink>
 	</div>
-	<RouterLink>
-		<img src="../assets/media/close.png" alt="Close image" class="close-img" />
-	</RouterLink>
-</div>
 </template>
 <style scoped>
 .header-chat {
@@ -22,6 +22,7 @@ import { RouterLink } from 'vue-router'
 	border-bottom: 1px solid rgba(0, 0, 0, 50%);
 	box-sizing: border-box;
 }
+
 .header-contact {
 	display: flex;
 	justify-content: center;
@@ -29,9 +30,11 @@ import { RouterLink } from 'vue-router'
 	gap: 20px;
 	font-size: 24px;
 }
+
 .contact-photo {
 	width: 40px;
 }
+
 .close-img {
 	width: 40px;
 }
---
+++ chatao-front-end/src/components/InputEmail.vue
@@ -0,0 +1,36 @@
+<script setup>
+import BoxEmail from './BoxEmail.vue';
+const props = defineProps({
+    text: String,
+    id: String,
+    placeholder: String,
+    name: String,
+    minlength: Number,
+    maxlength: String,
+    required: Boolean
+})
+</script>
+<template>
+<div class="box">
+    <div class="text"> {{ text }} </div>
+    <BoxEmail :id :placeholder :name :minlength :maxlength :required />
+</div>
+</template>
+<style scoped>
+.box {
+	box-sizing: border-box;
+    width: 100%;
+
+    display: flex;
+    padding: 15px;
+    gap: 15px;
+
+    justify-content: center;
+    align-items: center;
+}
+.text {
+	font-weight: 600;
+    color: white;
+    font-size: 24px;
+}
+</style>
