b6139a8dba3327b29948c4c0ef907c37ac7a8553
pdrzan <pedeozanelato19@gmail.com>
2024-06-17 20:42:46 -0400

    feat: Add getCookie function to main

--- chatao-front-end/src/main.js
+++ chatao-front-end/src/main.js
@@ -6,6 +6,11 @@ import router from './router'
 
 const app = createApp(App)
 
+window.getCookie = function (name) {
+    var match = document.cookie.match(new RegExp('(^| )' + name + '=([^;]+)'));
+    if (match) return match[2];
+}
+
 app.use(router)
 
 app.mount('#app')
