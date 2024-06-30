b61d4e4b0816c4ccbff7d71cecbc594d5e79b5ac
pdrzan <pedeozanelato19@gmail.com>
2024-06-30 19:41:56 -0400

    fix: remove pattern from email

--- chatao-front-end/src/components/BoxEmail.vue
+++ chatao-front-end/src/components/BoxEmail.vue
@@ -9,7 +9,7 @@ const props = defineProps({
 })
 </script>
 <template>
-    <input class="input" type="email" pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$" :id :placeholder :name
+    <input class="input" type="email" :id :placeholder :name
         :minlength :maxlength :required />
 </template>
 <style scoped>
